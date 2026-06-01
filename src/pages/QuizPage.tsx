30980e0c5f7a44cae8a0b6baa79ff9402149cfe1
Vaishnavivashistha416 <vaishnavivashistha416@gmail.com>
2026-06-01 14:31:38 +0530

    Create QuizPage component with welcome message

---
+++ src/pages/QuizPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuizPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Quiz Page</h1>
+            <p>Welcome to the quiz! Click the button to start answering questions.</p>
+        </div>
+    );
+};
+
+export default QuizPage;
